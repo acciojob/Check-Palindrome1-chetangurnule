51cf229bac4157d0635f51163205435d192dd143
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-03-16 07:49:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+let startIndex= 0;
+let endIndex = str.length;
+	while(startIndex < endIndex){
+		if(str[startIndex] != str[endIndex]){
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
