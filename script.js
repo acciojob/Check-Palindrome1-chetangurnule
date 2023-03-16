0b2d86a1a0dce0e5835712543630a43f37a3252e
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-03-16 07:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str1){
 let startIndex= 0;
+let str = str1.toLowerCase();
 let endIndex = str.length-1;
 	while(startIndex < endIndex){
 		if(str[startIndex] != str[endIndex]){
